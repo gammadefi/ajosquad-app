fe7691d8b80cab3b82dca8d004385172750927ec
emekaagu <aguchukwuemekag@gmail.com>
2024-11-21 17:22:56 +0100

    "Updated DashboardRoutes, GraphWrapper, InfoCard, Modal, and Payout components with new code and styling changes."

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,23 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CgProfile } from "react-icons/cg";
 import { LiaFileContractSolid } from "react-icons/lia";
 import { useAuth } from '../../zustand/auth.store'
 import clsx from 'clsx'
 import People from '../../icons/reusables/people';
+import TodayDate from '../../components/common/TodayDate';
+import { InfoCard } from '../../components/InfoCard/InfoCard';
+import { GraphWrapper } from '../../components/Graph/GraphWrapper';
+import LineChart from '../../components/Graph/Graphs/LineChart';
 
 const Dashboard = () => {
-  const [kycVerified, setKycVerified] = React.useState(false)
-  const [activeSquad, setActiveSquad] = React.useState(false)
+  const [kycVerified, setKycVerified] = React.useState(true)
+  const [activeSquad, setActiveSquad] = React.useState(true)
+
+  const allData: any = {
+    "24h": {
+      xAxisLabel: ["12AM", "4AM", "8AM", "12PM", "4PM", "8PM", "12AM"],
+      seriesData: [
+        { name: "Products", data: [10, 15, 8, 20, 14, 18, 22] },
+        { name: "Services", data: [12, 10, 18, 14, 16, 10, 20] },
+      ],
+    },
+    "7d": {
+      xAxisLabel: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      seriesData: [
+        { name: "Products", data: [30, 40, 45, 50, 49, 60, 70] },
+        { name: "Services", data: [13, 50, 42, 60, 34, 63, 43] },
+      ],
+    },
+    "6M": {
+      xAxisLabel: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+      seriesData: [
+        { name: "Products", data: [100, 90, 80, 85, 95, 110] },
+        { name: "Services", data: [70, 75, 60, 65, 55, 60] },
+      ],
+    },
+    "1Y": {
+      xAxisLabel: ["J", "F", "M", "A", "M", "J", "JY", "AG", "S", "O", "N", "D"],
+      seriesData: [
+        { name: "Products", data: [30, 40, 45, 50, 49, 60, 70, 30, 20, 50, 70, 100] },
+        { name: "Services", data: [13, 50, 42, 60, 34, 63, 43, 45, 50, 49, 60, 90] },
+      ],
+    },
+    Max: {
+      xAxisLabel: ["2019", "2020", "2021", "2022", "2023", "2024"],
+      seriesData: [
+        { name: "Products", data: [200, 250, 300, 350, 400, 450] },
+        { name: "Services", data: [150, 180, 210, 190, 220, 240] },
+      ],
+    },
+  };
+
+  const [selectedRange, setSelectedRange] = useState("1Y"); // Default range
+
+  // Get data for the selected range
+  const { xAxisLabel, seriesData } = allData[selectedRange];
+
   return (
 
 
     <div>
       {
-        (kycVerified === false || activeSquad === false) && (
+        (kycVerified === false || activeSquad === false) ? (
           <div>
             <Verification kycVerified={kycVerified} activeSquad={activeSquad} />
           </div>
-        )
+        ) :
+
+          <div className='px-3 md:px-6'>
+            <div className='flex justify-between items-center'>
+              <div>
+
+              </div>
+              <TodayDate />
+
+            </div>
+
+            <div className='grid my-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+              <InfoCard iconName='moneys-credit' value='CA$ 50,500.00' header='Total deposit' />
+              <InfoCard iconName='moneys-debit' value='CA$ 50,500.00' header='Total Withdrwal' />
+              <InfoCard type='squad' iconName='people' value='2' header='Squad' />
+
+            </div>
+
+            <div className='grid my-5 grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4'>
+              <div className='col-span-3'>
+                <GraphWrapper graphTitle="Overall Sale">
+                  <div className='flex items-center gap-3'>
+                    <div className="flex bg-[#F7F7F8] py-2 px-2 space-x-4 mb-4">
+                      {["24h", "7d", "6M", "1Y", "Max"].map((range) => (
+                        <button
+                          key={range}
+                          className={`px-4 py-2 bg-white rounded-lg ${selectedRange === range
+                              ? "border-blue-600 border text-gray-800"
+                              : " text-gray-800"
+                            }`}
+                          onClick={() => setSelectedRange(range)}
+                        >
+                          {range}
+                        </button>
+                      ))}
+                    </div>
+
+                  </div>
+                  <LineChart
+                    type='line'
+                    colors={["#0E8837", "#D42620",]}
+                    xAxisLabel={xAxisLabel}
+                    seriesData={seriesData}
+                  />
+                </GraphWrapper>
+              </div>
+
+            </div>
+
+
+          </div>
       }
     </div>
   )
@@ -39,7 +137,7 @@ const Verification = ({ kycVerified, activeSquad } = { kycVerified: false, activ
           <VerificationCards>
             <div className='flex gap-2'>
               <div className="mt-2">
-              <LiaFileContractSolid size={24} />
+                <LiaFileContractSolid size={24} />
               </div>
 
               <h3 className=" text-lg font-bold md:text-2xl">Read and sign the  Ajosquad Agreement</h3>
---
+++ src/pages/dashboard/Notifications.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import TabBar from '../../components/Tab/TabBar'
+import SearchInput from '../../components/FormInputs/SearchInput'
+import { GoStar, GoStarFill, GoTrash } from "react-icons/go";
+import { Paginator } from '../../components/Table/Paginator';
+
+const Notifications = () => {
+    const tabs = ["All", "Archived"]
+    return (
+        <div className='px-3  md:px-6'>
+            <div className='flex rounded-t-md bg-[#E6F0FF] px-3 h-[44px] gap-3 items-center'>
+                <img src='' alt='' />
+                <h3 className='text-base font-semibold'>Notification List</h3>
+            </div>
+
+            <div className='flex items-center my-8 justify-between'>
+                <h3 className='font-semibold '>{"188"} Notification</h3>
+                <div>
+                    <SearchInput placeholder='search...' />
+
+                </div>
+            </div>
+
+
+
+            <TabBar tabs={tabs} />
+            <div className='py-6 divide-y'>
+                <Notification isFavourite={false} />
+                <Notification isFavourite={true} />
+                <Notification isFavourite={false} />
+
+            </div>
+            <div className='flex justify-end'>
+                <Paginator page={1} pageSize={10} totalRows={188} loading={false} currentLength={188} />
+            </div>
+        </div>
+    )
+}
+
+const Notification = ({ isFavourite, }: { isFavourite: boolean }) => {
+    return (
+        <div className='h-[44px] flex items-center justify-between w-full'>
+            <div className='flex gap-2 items-center'>
+                <input type='checkbox' className='h-4 w-4 ' />
+                <button>{
+                    isFavourite ? <GoStarFill size={18} color='#F8DC0B' /> : <GoStar size={18} />
+                }</button>
+
+            </div>
+            <h3 className='font-semibold'>Welcome</h3>
+            <div >
+                <h3>Lorem ipsum dolor sit amet consectetur. et Adipiscing vel consequat ut sit molestie nd tincidunt risus faucibus. Pulvinar  risus risus...</h3>
+            </div>
+            <div className='flex gap-2 items-center'>
+                <h3>Just Now</h3>
+                <button className='h-6 flex justify-center items-center w-6 rounded-full bg-[#FBE9E9] '>
+                    <GoTrash color='#D42620' />
+                </button>
+
+            </div>
+
+
+
+        </div>
+    )
+}
+
+
+
+export default Notifications
