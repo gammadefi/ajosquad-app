e85a9e00836e59c60e36b588f26d8bf878a6ebfe
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-11-25 19:04:27 +0100

    fixed sqaud tab bug

--- src/pages/dashboard/Squad.tsx
+++ src/pages/dashboard/Squad.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useLocation } from 'react-router-dom';
 import SquadTabBar from '../../components/Tab/SquadTabBar';
 import SquadTypeTabBar from '../../components/Tab/SquadTypeTabBar';
