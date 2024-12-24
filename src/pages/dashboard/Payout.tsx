a3ccca01d55581da0417f8a9e9cef71035c214ae
emekaagu <aguchukwuemekag@gmail.com>
2024-12-24 16:52:52 +0100

    Refactor Payout component layout and remove unused code

--- src/pages/dashboard/Payout.tsx
+++ src/pages/dashboard/Payout.tsx
@@ -81,48 +81,8 @@ const Payout = () => {
 
     return (
         <div className='px-3  md:px-6'>
-            <>
-                <div>
-                    <InfoCard iconName='moneys-debit' value='CA$ 50,500.00' header='Total Withdrwal' />
-                </div>
 
-                <h3 className='mt-8 text-[#0000006B] text-sm '><span className='text-[#000] mr-2 font-semibold text-xl '>Payout</span>  Track your payouts easily. See every transaction in one place.</h3>
 
-                <div className='my-8 flex flex-col lg:flex-row gap-3 justify-between lg:items-center'>
-                    <div className='flex justify-between'>
-                        <h3 className='text-xl font-semibold'>All Payout  Transaction</h3>
-                        <button className='lg:hidden text-primary px-4 py-2 border border-primary rounded-lg font-semibold'>Download</button>
-                    </div>
-                    <div className='flex items-center gap-2'>
-                        <SearchInput value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Search...' />
-                        <button onClick={() => setOpenFilter(true)} className='bg-[#F5F5F9] w-full md:w-1/5 lg:w-full flex items-center justify-center gap-2 border-[0.4px] border-[#C8CCD0] text-[#666666] py-2 px-3 rounded-md'>
-                            <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect width="20" height="3.33333" transform="translate(0 1.66797)" fill="#464749" />
-                                <rect width="20" height="3.33333" transform="translate(0 8.33203)" fill="#464749" />
-                                <rect width="20" height="3.33333" transform="translate(0 15)" fill="#464749" />
-                                <rect x="3.07617" width="3.07692" height="6.66667" rx="1.53846" fill="#464749" />
-                                <rect x="3.07617" y="13.332" width="3.07692" height="6.66667" rx="1.53846" fill="#464749" />
-                                <rect x="14.6133" y="6.66797" width="3.07692" height="6.66667" rx="1.53846" fill="#464749" />
-                            </svg>
-                            <span>
-                                Filter By
-                            </span>
-                        </button>
-                        <button className='hidden lg:block text-primary px-4 py-2 border border-primary rounded-lg font-semibold'>Download</button>
-                    </div>
-                    <Filter filterBy={["amount", "date", "position", "squad", "status"]} open={openFilter} onClose={() => setOpenFilter(false)} />
-                </div>
-                {
-                    payouts && payouts.data.length === 0 ? <TableEmpty title='Payout History Details' image='/empty-states/payout.png' subtitle="On this page, you'll find a record of your previous payouts, including dates, and amount." /> : <Table
-                        data={payouts.data}
-                        columns={columns}
-                        loading={false}
-                        pagination={
-                            {
-                                page: currentPage,
-                                setPage: setCurrentPage,
-                                totalRows: payouts.pagination.pages,
-                            }
             {
                 false ? <PageLoader />
 
@@ -177,9 +137,9 @@ const Payout = () => {
                                 />
                         }
 
-                    />
-                }
-            </>
+                    </>
+
+            }
         </div>
     )
 }
